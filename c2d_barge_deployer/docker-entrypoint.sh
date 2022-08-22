505deb6649a7cc4edd687d88a85d4f69f2018156
Călina Cenan <calina@cenan.net>
2022-08-22 15:55:14 +0300

    Update docker-entrypoint.sh

--- c2d_barge_deployer/docker-entrypoint.sh
+++ c2d_barge_deployer/docker-entrypoint.sh
@@ -88,7 +88,7 @@ echo "Waiting for op-service deployment, so we can init pgsql"
 kubectl wait -n ocean-operator deploy/operator-api --for=condition=available --timeout 10m
 sleep 10
 #initialize op-api
-until $(curl --output /dev/null --silent --head --fail -X POST "http://${KIND_IP}:31000/api/v1/operator/pgsqlinit" -H  "accept: application/json"); do
+until $(curl --output /dev/null --silent --head --fail -X POST "http://${KIND_IP}:31000/api/v1/operator/pgsqlinit" -H  "accept: application/json" -H "Admin: myAdminPass"); do
     printf '.'
     sleep 5
 done
